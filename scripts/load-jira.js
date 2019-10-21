0e0894b0294da09c154eb3fbf300657e8aa25ab2
vnbt <vnbatov@wiley.com>
2019-10-21 21:03:04 +0300

    #23 load stories with tasks

--- scripts/load-jira.js
+++ scripts/load-jira.js
@@ -10,8 +10,27 @@ const jira = new JiraClient({ host, basic_auth: { username, password } })
 const getIssuesByFilter = async (jql) => jira.search.search({ jql })
 
 const main = async () => {
-  const result = await getIssuesByFilter('project=CPP0 and issuetype=Story and Sprint="FY20 Sprint 8"')
-  dbJSON.stories = result.issues.map(({ key, fields: { summary } }) => ({ id: key, summary }))
+  const result = await getIssuesByFilter('project=CPP0 and issuetype=Story and Sprint="FY20 Sprint 8" and key=CPP0-1548')
+  dbJSON.stories = result.issues.map(({ key, fields: { summary, customfield_11220: epicId } }) => ({ id: key, summary, epicId }))
+  result.issues.forEach(issue => {
+    const tasks = issue.fields.issuelinks.filter(link => link.type.name === 'Task' && link.inwardIssue).map(link => ({
+      id: link.inwardIssue.key,
+      summary: link.inwardIssue.fields.summary,
+      story: issue.key,
+      related: '',
+      sp: ''
+    }))
+
+    tasks.forEach(task => {
+      const teamId = task.id.split('-')[0]
+      const sprint = dbJSON.sprints.find(({ id }) => id === teamId)
+      console.log(teamId, sprint && sprint.teamName)
+      if (sprint) {
+        dbJSON.tasks.push({ ...task, teamName: sprint.teamName })
+        sprint.columns['column-1'].taskIds.push(task.id)
+      }
+    })
+  })
   fs.writeFile(dbFileName, JSON.stringify(dbJSON), function (err) {
     if (err) return console.log(err)
     console.log('writing to ' + dbFileName)
